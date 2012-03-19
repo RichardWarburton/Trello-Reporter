39da1162594c75a3e6f74308563bc55f8d03479b
Richard Warburton <richard.warburton@gmail.com>
2012-03-19 15:01:34 +0000

    initial commit

---
+++ trello-reporter/lib.py
@@ -0,0 +1,45 @@
+from trollop import TrelloConnection
+from settings import *
+from datetime import datetime, timedelta
+import pystache
+import re
+
+# NB: also includes actions from the past, deliberate behaviour
+one_week = timedelta(days=7)
+before = datetime.now() + one_week
+conn = TrelloConnection(TRELLO_KEY, USER_TOKEN)
+
+if locals().has_key('ONLY_BOARD'):
+    check = lambda board: board.name == 'Personal Todo'
+else:
+    check = lambda board: True
+
+class BoardView(pystache.View):
+    
+    template_name = 'plaintext'
+
+    def boards(self):
+        for board in conn.me.boards:
+            if not board.closed and check(board):
+                print board.name
+                tasks = self._get_tasks(board)
+                yield { 'tasks': tasks,
+                        'empty': not tasks,
+                        'board_name': board.name, }
+        
+    def _get_tasks(self, board):
+        tasks = []
+        for card in board.cards:
+            if not card.closed and card._data.has_key('due') and conn.me.username in [m.username for m in card.members]:
+                due = datetime(*map(int, re.split('[^\d]', card._data['due'])[:-1]))
+                if due <= before:
+                    tasks.append({'msg': card.name, 'date': due})
+        return tasks
+
+    def username(self):
+        return conn.me.fullname
+
+if __name__ == "__main__":
+    print BoardView().render()
+
+    # TODO: Email to USER_EMAIL
---
+++ trello-reporter/settings-default.py
@@ -0,0 +1,9 @@
+
+'''See https://trello.com/1/appKey/generate#'''
+
+TRELLO_KEY = "Insert your app key here"
+TRELLO_TOKEN = "Insert your user token here"
+USER_EMAIL = "Insert your email here"
+
+# Optional Settings:
+# ONLY_BOARD = "Insert your "
